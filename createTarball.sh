10e7e721e1467e3db482a4ef0f23628c09b75ed6
Alexander Schwinn <al.schwinn@gsi.de>
2018-06-01 12:45:43 +0000

    added scripts to create tarballs
    git-svn-id: https://www-acc.gsi.de/svn/fesa/device/driver/gr-flowgraph/trunk@17598 243f544d-f450-4db5-8dff-af105d85f54e

---
+++ createTarball.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+FOLDER_TO_TAR=usr
+INSTALL_DIR_LIB=${FOLDER_TO_TAR}/lib64
+INSTALL_DIR_BIN=${FOLDER_TO_TAR}/bin
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")     # path where this script is located in
+
+TARBALL_NAME=Flowgraph.tar
+
+mkdir -p ${INSTALL_DIR_LIB}
+mkdir -p ${INSTALL_DIR_BIN}
+
+cp ${SCRIPTPATH}/build/lib/libgnuradio-flowgraph-1.0.0.svn.so.0.0.0 ${INSTALL_DIR_LIB}
+
+cp ${SCRIPTPATH}/build/lib/test-flowgraph ${INSTALL_DIR_BIN}
+cp ${SCRIPTPATH}/build/lib/test_flowgraph_test.sh ${INSTALL_DIR_BIN}
+
+tar cfv ${TARBALL_NAME} ${FOLDER_TO_TAR}
+rm -rf ${FOLDER_TO_TAR}
+gzip ${TARBALL_NAME}
+
+scp ${TARBALL_NAME}.gz asl744:/common/export/fesa/arch/x86_64
