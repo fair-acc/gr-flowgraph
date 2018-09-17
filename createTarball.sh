631d8dceeb5bd9ee4c28a4c84237635d72423aa8
aschwinn <al.schwinn@gsi.de>
2018-09-17 10:23:14 +0200

    added versioning to released objects

--- createTarball.sh
+++ createTarball.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]
+  then
+    echo "Error: No arguments supplied. First and only argument has to be Version. TODO: Automatically get version"
+    exit 1
+fi
+if [ $# -ne 1 ]
+  then
+    echo "Error: Wrong number arguments supplied. First and only argument has to be Version TODO: Automatically get version"
+    exit 1
+fi
+
+VERSION=$1
 FOLDER_TO_TAR=usr
 INSTALL_DIR_LIB=${FOLDER_TO_TAR}/lib64
 INSTALL_DIR_BIN=${FOLDER_TO_TAR}/bin
@@ -7,12 +19,12 @@ INSTALL_DIR_BIN=${FOLDER_TO_TAR}/bin
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")     # path where this script is located in
 
-TARBALL_NAME=Flowgraph.tar
+TARBALL_NAME=Flowgraph-${VERSION}.tar
 
 mkdir -p ${INSTALL_DIR_LIB}
 mkdir -p ${INSTALL_DIR_BIN}
 
-cp ${SCRIPTPATH}/build/lib/libgnuradio-flowgraph-*.so.0.0.0 ${INSTALL_DIR_LIB}
+cp ${SCRIPTPATH}/build/lib/libgnuradio-flowgraph-${VERSION}.master.so.0.0.0 ${INSTALL_DIR_LIB}
 
 cp ${SCRIPTPATH}/build/lib/test-flowgraph ${INSTALL_DIR_BIN}
 cp ${SCRIPTPATH}/build/lib/test_flowgraph_test.sh ${INSTALL_DIR_BIN}
