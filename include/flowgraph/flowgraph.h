7d76a064e6f64f9c6fa53b15d7f8bbbe8c23b47e
aschwinn <al.schwinn@gsi.de>
2019-03-01 15:27:36 +0100

    Find a reliable way to match FESA wr-events and picoscope triggers https://gitlab.com/al.schwinn/gr-digitizers/issues/28 - Dont use queue + lock to stor wr-events, use a cyclic buffer instead

--- include/flowgraph/flowgraph.h
+++ include/flowgraph/flowgraph.h
@@ -403,14 +403,17 @@ public:
         return nullptr;
     }
 
-    void post_timing_event(const std::string &event_code, int64_t wr_trigger_stamp, int64_t wr_trigger_stamp_utc)
+    bool post_timing_event(const std::string &event_code, int64_t wr_trigger_stamp, int64_t wr_trigger_stamp_utc)
     {
+        bool success = true;
         for (const auto &elem : d_block_map) {
             if (elem.second.type == time_realignment_key) {
                 auto block = boost::dynamic_pointer_cast<gr::digitizers::time_realignment_ff>(elem.second.block);
-                block->add_timing_event(event_code, wr_trigger_stamp, wr_trigger_stamp_utc);
+                if( block->add_timing_event(event_code, wr_trigger_stamp, wr_trigger_stamp_utc) == false )
+                        success = false;
             }
         }
+        return success;
     }
 
 private:
